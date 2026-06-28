463a8cbf806965eb1dafb1b55f229bbcdfaa7a49
Offranel <francis.sengele@gmail.com>
2026-06-27 21:42:17 -0700

    Add UI components for an area and perimeter calculator

--- WinFormsSourceControl/Form1.Designer.cs
+++ WinFormsSourceControl/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 41);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 161);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 41);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(152, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(250, 47);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(152, 158);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(250, 47);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(186, 283);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 58);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
